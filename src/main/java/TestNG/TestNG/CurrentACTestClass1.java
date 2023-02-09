cfc24cbf5bd637e5d1b32a50cacafbcabadc46ff
rupesh <rupesh25112000@gmail.com>
2023-02-09 06:51:29 +0530

    TEST_NG_XML_FILE

---
+++ src/main/java/TestNG/TestNG/CurrentACTestClass1.java
@@ -0,0 +1,17 @@
+package TestNG.TestNG;
+import org.testng.annotations.*;
+
+public class CurrentACTestClass1 {
+
+    @Test
+    public void test1()
+    {
+        System.out.println("CURRENT A/C TESTCASE1");
+    }
+
+    @Test
+    public void test2()
+    {
+        System.out.println("CURRENT A/C TESTCASE2");
+    }
+}
---
+++ src/main/java/TestNG/TestNG/CurrentACTestClass2.java
@@ -0,0 +1,17 @@
+package TestNG.TestNG;
+import org.testng.annotations.*;
+
+public class CurrentACTestClass2 {
+
+    @Test
+    public void test1()
+    {
+        System.out.println("Current A/C testcase no. 1");
+    }
+
+    @Test
+    public void test2()
+    {
+        System.out.println("Current A/C testcase no. 2");
+    }
+}
